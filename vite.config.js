9ff19be7601c3cdceb2e50b77bf75f81e548f5ac
codeask10 <annajmussaquib123@gmail.com>
2024-10-26 23:25:05 +0530

    Remove proxy api base url

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,5 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    proxy: {
-      "/api": {
-        target: "https://restcountries.com/v3.1",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      }
-    }
   },
 })
